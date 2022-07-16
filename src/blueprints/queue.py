4c9bc88d4c182c90c1bb77011cf12011d731038f
firdausraginda <ragindafirdaus01@gmail.com>
2022-07-16 19:03:14 +0700

    GET & POST using same route for queue blueprints; create get by id using user auth

--- src/blueprints/queue.py
+++ src/blueprints/queue.py
@@ -1,22 +1,79 @@
+from asyncio import QueueEmpty
 from flask import Blueprint, request, jsonify
 from src.constants.http_status_code import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_404_NOT_FOUND
-from src.database import Queue, Admin, db
+from src.database import Queue, Admin, QueueUser, User, db
 from src.auth.auth_admin import auth_admin
 from src.auth.auth_user import auth_user
 
 
 queue = Blueprint("queue", __name__, url_prefix="/api/v1/queue")
 
-@queue.get("/get", defaults={"id": None})
-@queue.get("/get/<int:id>")
+@queue.route("/", defaults={"id": None}, methods=["POST", "GET"])
+@queue.route("/<int:id>", methods=["POST", "GET"])
 @auth_admin.login_required
-def get_queue_by_admin(id):
+def get_queue_by_auth_admin(id):
 
     admin_result = Admin.query.filter_by(email=auth_admin.current_user()).first()
-    filters = (Queue.admin_id == admin_result.id,)
-    if id:
-        filters = filters + ((Queue.id == id),)
-    queue_result = Queue.query.filter(*filters).all()
+
+    if request.method == "GET":
+        filters = (Queue.admin_id == admin_result.id,)
+        if id:
+            filters = filters + ((Queue.id == id),)
+        queue_result = Queue.query.filter(*filters).all()
+
+        if not queue_result:
+            return jsonify({
+                "message": "item not found!"
+            }), HTTP_404_NOT_FOUND
+
+        data = []
+        for queue in queue_result:
+            data.append({
+                "id": queue.id,
+                "name": queue.name,
+                "created_at": queue.created_at,
+                "updated_at": queue.updated_at,
+                "description": queue.description,
+                "status": queue.status,
+                "short_url": queue.short_url,
+                "admin_id": queue.admin_id
+            })
+        
+        return jsonify({
+            "data": data
+        }), HTTP_200_OK
+
+    else:
+        body_data = request.get_json()
+
+        queue = Queue(
+            name = body_data.get("name"),
+            description = body_data.get("description"),
+            status = body_data.get("status"),
+            short_url = body_data.get("short_url"),
+            admin_id = admin_result.id
+        )
+
+        try:
+            db.session.add(queue)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            raise
+        
+        return jsonify({
+            "name": body_data.get("name"),
+            "description": body_data.get("description"),
+            "status": body_data.get("status"),
+            "admin_id": admin_result.id
+        }), HTTP_201_CREATED
+
+@queue.get("/list")
+@auth_user.login_required
+def get_queue_by_auth_user():
+    user_result = User.query.filter_by(email=auth_user.current_user()).first()
+    queue_user_result = QueueUser.query.filter_by(user_id=user_result.id).all()
+    queue_result = Queue.query.filter(Queue.id.in_([queue_user.queue_id for queue_user in queue_user_result])).all()
 
     data = []
     for queue in queue_result:
@@ -34,35 +91,7 @@ def get_queue_by_admin(id):
     return jsonify({
         "data": data
     }), HTTP_200_OK
-        
-@queue.post("/post")
-@auth_admin.login_required
-def post_queue_by_admin():
-    admin_result = Admin.query.filter_by(email=auth_admin.current_user()).first()
-    body_data = request.get_json()
 
-    queue = Queue(
-        name = body_data.get("name"),
-        description = body_data.get("description"),
-        status = body_data.get("status"),
-        short_url = body_data.get("short_url"),
-        admin_id = admin_result.id
-    )
-
-    try:
-        db.session.add(queue)
-        db.session.commit()
-    except:
-        db.session.rollback()
-        raise
-    
-    return jsonify({
-        "name": body_data.get("name"),
-        "description": body_data.get("description"),
-        "status": body_data.get("status"),
-        "admin_id": admin_result.id
-    }), HTTP_201_CREATED
-            
 @queue.delete("/<int:id>")
 @auth_admin.login_required
 def delete_queue(id):
